ac2b914d306e21c558b46c25c1eae454e4f7071c
swapnil-mirgane <jigarswapnil143@gmail.com>
2022-08-18 16:36:40 +0530

    fix: tailwind error resolve

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/assets/css/tailwind.css'],
+  
     build: {
       postcss: {
-        postcssOptions: require('./postcss.config.js'),
-      }
-    }
+        postcssOptions: {
+          plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+          },
+        },
+      },
+    },
+    css:[
+      "~/assets/css/tailwind.css"
+    ],
+    
 })
