8a139ef5127656d942e616983729e1ae5479176d
Amit01 <amitkumar1437786@gmail.com>
2022-08-23 15:47:43 +0530

    fix:all api error fix

--- pages/Product.vue
+++ pages/Product.vue
@@ -1,139 +1,223 @@
 <template>
-    <div>
-        <h1 class="font-bold text-center text-2xl mt-10 text-green-400">Product Management</h1>
-        <div class="  grid grid-cols-2  ">
-            <div class="border-black  border-2 m-8 p-8">
-                <form @submit="formSubmit" class="bg-green-100 border-green-400 rounded-lg border-2 px-12">
-                    <table>
-                        <h2 class="text-teal-900 text-xl font-bold pt-6">“Add Product” </h2>
-                        <hr />
-                        <label class="pt-10 py-10 " for="ProductName">Product Name:</label><br />
-                        <input type="text" v-model="user.ProductName" id="ProductName" name="ProductName"
-                            placeholder="" /><br /><br />
-                        <label for="Price">Price:</label><br />
-                        <input type="number" v-model="user.Price" id="Price" name="Price" placeholder="" /><br /><br />
-                        <label for="state">Stock:</label>
-                        <select class="p-1" name="Stock" id="Stock" v-model="user.Stock">
-                            <option value="Available" selected>Available</option>
-                            <option value="Out of Stock ">Out of Stock </option>
-                        </select><br /><br />
-                        <label for="state">Size:</label>
-                        <select class="p-1" name="Size" id="Size" v-model="user.Size">
-                            <option value="XS">XS</option>
-                            <option value="S ">S</option>
-                            <option value="M" selected>M</option>
-                            <option value="L">L</option>
-                            <option value="XL">XL</option>
-                        </select><br><br>
-                        <label for="Image">Upload Image</label><br><br>
-                        <input type="file" v-bind="user.Image" id="Image" name="Image"><br><br>
-                        <div class="mt-10">
-                            <button
-                                class="py-1 px-5 mr-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
-                                type="submit" @click="formSubmit">Add Product</button>
-                            <button
-                                class="py-1 px-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
-                                type="reset"> Reset </button>
-                        </div>
-                    </table>
-                </form>
-            </div>
-            <div class="border-black  border-2 m-8 p-8">
-                <table class="list">
-                    <tr>
-                        <!-- <th class="px-4 border-black rounded-lg border-2">id</th> -->
-                        <th class="px-4 border-green-400 rounded-lg border-4">ProductName</th>
-                        <th class="px-4 border-green-400 rounded-lg border-4">Price</th>
-                        <th class="px-4 border-green-400 rounded-lg border-4">Stock</th>
-                        <th class="px-4 border-green-400 rounded-lg border-4">Size</th>
-                        <th class="px-4 border-green-400 rounded-lg border-4">Image</th>
-                        <th class="px-4 border-green-400 rounded-lg border-4">Action</th>
-                    </tr>
-                    <tr v-for="(item, index) in users" v-bind:index="index" :key="item">
-                        <!-- <td class="px-4 border-black rounded-lg border-2">{{item.id=i+1}}</td> -->
-                        <td class="px-4 border-green-400 rounded-lg border-4">{{ item.ProductName }}</td>
-                        <td class="px-4 border-green-400 rounded-lg border-4">{{ item.Price }}</td>
-                        <td class="px-4 border-green-400 rounded-lg border-4">{{ item.Stock }}</td>
-                        <td class="px-4 border-green-400 rounded-lg border-4">{{ item.Size }}</td>
-                        <td class="px-4 border-green-400 rounded-lg border-4">
-                            <img src="{{item.Image}}" alt="" srcset="">
-                        </td>
-                        <td class="px-4 border-green-400 rounded-lg border-4">{{ item.Action }}
-                            <button class="mx-3 rounded-lg bg-red-600 hover:bg-red-700 text-white w-20"
-                                @click="userDelete(index)">
-                                Delete
-                            </button>
-                            <button class="mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20"
-                                @click="onEdit(index)">
-                                Edit
-                            </button>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
+  <div>
+    <h1 class="font-bold text-center text-2xl mt-10 text-green-400">
+      Product Management
+    </h1>
+    <!-- <div class="grid grid-cols-2"> -->
+    <div class="border-black border-2 m-8 p-8">
+      <!-- @submit="onFormSubmit1()" -->
+      <form class="bg-green-100 border-green-400 rounded-lg border-2 px-12">
+        <table>
+          <h2 class="text-teal-900 text-xl font-bold pt-6">“Add Product”</h2>
+          <hr />
+          <label class="pt-10 py-10" for="ProductName">Product Name:</label
+          ><br />
+          <input
+            v-model="mydata.product.productName"
+            type="text"
+            ref="productName"
+            id="ProductName"
+            name="ProductName"
+            placeholder=""
+          /><br /><br />
+          <label for="Price">Price:</label
+          ><br />
+          <input
+            v-model="mydata.product.price"
+            type="number"
+            ref="price"
+            id="Price"
+            name="Price"
+            placeholder=""
+          /><br /><br />
+          <label for="Stock">Stock:</label>
+          <select
+            v-model="mydata.product.stock"
+            class="p-1"
+            name="Stock"
+            id="Stock"
+            ref="stock"
+          >
+            <option value="Available" selected>Available</option>
+            <option value="Out of Stock ">Out of Stock</option></select
+          ><br /><br />
+          <label for="Size">Size:</label>
+          <select
+            v-model="mydata.product.size"
+            class="p-1"
+            name="Size"
+            id="Size"
+            ref="size"
+          >
+            <option value="XS">XS</option>
+            <option value="S">S</option>
+            <option value="M" selected>M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option></select
+          ><br /><br />
+          <label for="Image">Upload Image</label
+          ><br /><br />
+          <input type="file" id="Image" name="Image" /><br /><br />
+          <div class="mt-10">
+            <button
+              class="py-1 px-5 mr-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
+              type="button"
+              @click="onFormSubmit1()"
+            >
+              Add Product
+            </button>
+            <button
+              class="py-1 px-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3"
+              type="reset"
+            >
+              Reset
+            </button>
+          </div>
+        </table>
+      </form>
     </div>
+    <div class="border-black border-2 m-8 p-8">
+      <table class="list">
+        <tr>
+          <!-- <th class="px-4 border-black rounded-lg border-2">id</th> -->
+          <th class="px-4 border-green-400 rounded-lg border-4">ProductId</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">ProductName</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Price</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Stock</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Size</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Image</th>
+          <th class="px-4 border-green-400 rounded-lg border-4">Action</th>
+        </tr>
+        <tr v-for="item in mydata.allProduct" :key="item.id">
+          <!-- <td class="px-4 border-black rounded-lg border-2">{{item.id=i+1}}</td> -->
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.id }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.productName }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.price }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.stock }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.size }}
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            <img src="{{item.image}}" alt="" srcset="" />
+          </td>
+          <td class="px-4 border-green-400 rounded-lg border-4">
+            {{ item.Action }}
+            <button
+              class="mx-3 rounded-lg bg-red-600 hover:bg-red-700 text-white w-20"
+              @click="onDeleteOfProduct(item.id)"
+            >
+              Delete
+            </button>
+            <button
+              class="mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20"
+              @click="editProduct(item.id)"
+            >
+              Edit
+            </button>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <!-- </div> -->
+    <!-- <p>{{ allProduct }}</p> -->
+  </div>
 </template>
-<script>
-export default {
-    data() {
-        return {
-            isEdit: false,
-            indexEdit: -1,
-            users: [],
-            userAddress: '',
-            user: {
-                ProductName: '',
-                Price: '',
-                Stock: '',
-                Size: '',
-                Image: '',
-            },
-        };
-    },
-    methods: {
-        formSubmit(event) {
-            event.preventDefault();
-            // if(this.user.ProductName==""||this.user.ProductName==null||this.user.Price==""|| this.user.Price==null|| this.user.Stock==""||this.user.Stock==null|| this.user.Color==""||this.user.Color==null)
-            // {alert("please fill all the fields");
-            // this.onReset();
-            // }else{console.log("Hi");}
-            if (this.isEdit == true) {
-                this.users[this.indexEdit] = this.user;
-                this.isEdit = false;
-                this.indexEdit = -1;
-            }
-            else {
-                this.users.push(this.user);
-            }
-            this.user = {
-                ProductName: '',
-                Price: '',
-                Stock: '',
-                Size: '',
-                Image: '',
-            };
-        },
-        onReset(event) {
-            event.preventDefault();
-            this.form.ProductName = "";
-            this.form.Price = "";
-            this.form.Stock = "";
-            this.form.Size = "";
-            this.form.Image = "";
-        },
-        userDelete(index) {
-            this.users.splice(index, 1)
-        },
-        onEdit(index) {
-            this.user.ProductName = this.users[index].ProductName;
-            this.user.Price = this.users[index].Price;
-            this.user.Stock = this.users[index].Stock;
-            this.user.Size = this.users[index].Size;
-            this.user.Image = this.users[index].Image;
-            this.isEdit = true;
-            this.indexEdit = index;
-        },
+<script setup lang="ts">
+// import { ref } from "vue";
+// const productName = ref("");
+// const price = ref("");
+// const stock = ref("");
+// const size = ref("");
+// const emp_contact = ref("");
+// const emp_salary = ref("");
+// const emp_dept = ref("");
+
+// const productName =
+
+const mydata = reactive({
+  allProduct: [],
+  product: {
+    productName: "",
+    price: "",
+    stock: "",
+    size: "",
+    // emp_contact: '',
+    // emp_salary: '',
+    // emp_dept: '',
+  },
+});
+
+getProductAPI();
+
+// GET API
+async function getProductAPI() {
+  mydata.allProduct = await $fetch("http://localhost:8080/product/allData");
+}
+
+// POST API
+async function onFormSubmit1() {
+  console.log(mydata.product);
+
+  await $fetch("http://localhost:8080/product", {
+    method: "POST",
+    body: JSON.stringify(mydata.product),
+  });
+  getProductAPI();
+}
+// // PATCH API
+async function onClickOfEditProduct(id) {
+  const sampleData = {
+    id: id,
+    productName: "Shaktiman" + id,
+    price: "ankita" + mydata.allProduct.length,
+    stock: 200 + mydata.allProduct.length,
+    size: "ghjgj" + mydata.allProduct.length,
+    image: "91001" + mydata.allProduct.length,
+  };
+  // const response = await $fetch('http://localhost:8080/product/' + id, {
+  //     method: 'PATCH',
+  //     body: JSON.stringify(sampleData),
+  // });
+  // getBookAPI();
+  getProductAPI();
+}
+async function editProduct(id) {
+  let productEdit = mydata.product.filter(
+    (product: {
+      id: any;
+      productName: any;
+      price: any;
+      stock: any;
+      size: any;
+    }) => {
+      if ((product.id = id)) {
+        mydata.product.id = product.id;
+        mydata.product.productName = product.productName;
+        mydata.product.price = product.price;
+        mydata.product.stock = product.stock;
+        mydata.product.size = product.size;
+      }
     }
+  );
+  console.log(productEdit);
+  const response = await $fetch("http://localhost:8080/product/" + id, {
+    method: "PATCH",
+    body: JSON.stringify(mydata.product),
+  });
+}
+// // Delete API
+async function onDeleteOfProduct(id) {
+  await $fetch("http://localhost:8080/product/" + id, {
+    method: "DELETE",
+  });
+  getProductAPI();
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <!-- <div class="px-72 pt-4"><img src="../hicare.png" alt="Logo" width="100" height="50"> </div> -->
     <p class="m-auto text-xl font-bold">Group E Frontend</p>
   </nav>
-  <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+  <div
+    class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5"
+  >
     <!--Card book 1-->
     <div class="rounded overflow-hidden shadow-lg">
       <div class="px-6 py-4">
@@ -13,8 +15,9 @@
           </NuxtLink>
         </div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia,
-          Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
         </p>
       </div>
     </div>
@@ -27,8 +30,9 @@
           </NuxtLink>
         </div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia,
-          Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
         </p>
       </div>
     </div>
@@ -43,8 +47,9 @@
           </NuxtLink>
         </div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia,
-          Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
         </p>
       </div>
     </div>
@@ -59,8 +64,9 @@
           </NuxtLink>
         </div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia,
-          Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
         </p>
       </div>
     </div>
@@ -69,13 +75,14 @@
     <div class="rounded overflow-hidden shadow-lg">
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">
-          <NuxtLink to="/Product" class="hover:text-red-600 mr-2">
+          <NuxtLink to="/product" class="hover:text-red-600 mr-2">
             Product Management
           </NuxtLink>
         </div>
         <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia,
-          Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
         </p>
       </div>
     </div>
